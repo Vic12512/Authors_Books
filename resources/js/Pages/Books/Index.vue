77016f897bf9a462f1a5353cdc53b77997bb8237
VeDev12512 <ve.cuevasr12512@gmail.com>
2025-07-21 01:19:21 -0400

    Crea vista de libros

---
+++ resources/js/Pages/Books/Index.vue
@@ -0,0 +1,48 @@
+<template>
+    <title>Index</title>
+
+    <div>
+        <h1>Vista de Libros</h1>
+
+        <a href="/books/create">Crear Autor</a>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Fecha de publicación</td>
+                        <td>Edición</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr v-for="book in books" :key="author.id">
+                        <td>{{ book.name }}</td>
+                        <td>{{ book.publication_date }}</td>
+                        <td>{{ book.edition }}</td>
+                        <td>
+                            <Link :href="`/books/${book.id}/edit`">Editar</Link>
+                        </td>
+                        <td>
+                            <button @click="softDestroyBooks(books.id)">Ocultar</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+    import { Link, router } from '@inertiajs/vue3';
+
+    defineProps({
+        books: Array
+    });
+
+    function softDestroyBooks(id) {
+        router.delete(`/books/${id}`);
+    }
+
+</script>
