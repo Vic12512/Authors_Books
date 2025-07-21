fd275efadefc1ffd1839f2236d3d4d200a433cfb
VeDev12512 <ve.cuevasr12512@gmail.com>
2025-07-21 01:36:31 -0400

    Crea rutas de libro

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('a
 Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update');
 Route::delete('/authors/{author}', [AuthorController::class, 'softDestroy'])->name('authors.softDestroy');
 
-
+//rutas de Book
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{book}', [BookController::class, 'softDestroy'])->name('books.softDestroy');
