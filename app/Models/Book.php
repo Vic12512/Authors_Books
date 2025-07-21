c095c3157cc7b54f9ec55c4e8c7f8b8fb92454d7
VeDev12512 <ve.cuevasr12512@gmail.com>
2025-07-21 01:09:51 -0400

    Configura Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    //Campos que pueden ser asignados masivamente
+    protected $fillable = [
+        'name',
+        'publication_date',
+        'edition',
+    ];
 }
