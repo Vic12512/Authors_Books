da7859517f5a8dfc3935b0a87c8ee38ef20b4c64
VeDev12512 <ve.cuevasr12512@gmail.com>
2025-07-21 01:31:53 -0400

    Crea vistas de libro

---
+++ resources/js/Pages/Books/Create.vue
@@ -0,0 +1,35 @@
+<template>
+    <title>Create</title>
+
+    <div>
+        <h1>Crear Libros</h1>
+        <form @submit.prevent="submit">
+            <input v-model="name" type="text" placeholder="Nombre" name="Nombre" id="Nombre">
+            <input v-model="publication_date" type="date" placeholder="Fecha de publicación" name="Fecha de publicación" id="Fecha de publicación">
+            <input v-model="edition" type="text" placeholder="Edición" name="Edición" id="Edición">
+            <button type="submit">Guardar</button>
+
+        </form>
+    </div>
+</template>
+
+<script setup>
+    //importaciones
+    import {ref} from 'vue';
+    import {router} from '@inertiajs/vue3';
+
+    //Datos
+    const name = ref('');
+    const publication_date = ref('');
+    const edition = ref('');
+    
+    //funciones
+    function submit(){
+        router.post('/authors', {
+            name: name.value,
+            last_name: publication_date.value,
+            country: edition.value,
+        })
+    }
+
+</script>
---
+++ resources/js/Pages/Books/Edit.vue
@@ -0,0 +1,44 @@
+<template>
+    <title>Edit</title>
+
+    <div>
+        <h1>Editar Libros</h1>
+        <form @submit.prevent="submit">
+            <label for="Nombre">Nombre</label>
+            <input 
+                v-model="form.name" type="text" name="Nombre" id="Nombre">
+
+            <label for="Fecha de publicació">Fecha de publicación</label>
+            <input v-model="form.publication_date" type="date" name="Fecha de publicación" id="Fecha de publicación">
+
+            <label for="Edición">Edición</label>
+            <input v-model="form.edition" type="text" name="Edición" id="Edición">
+            <button type="submit">Actualizar</button>
+
+        </form>
+    </div>
+</template>
+
+<script setup>
+    //importaciones
+    import {useForm} from '@inertiajs/vue3';
+    import {router} from '@inertiajs/vue3';
+    import { defineProps } from 'vue';
+
+    //Datos
+    const props = defineProps({
+        book: Object
+    });
+
+    const form = useForm({
+        name: props.author.name,
+        publication_date: props.author.publication_date,
+        edition: props.author.edition
+    })
+    
+    //funciones
+    function submit(){
+        form.put(`/authors/${props.book.id}`)
+    }
+
+</script>
