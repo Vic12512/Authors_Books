c4915a061f0460628383011959926328ec7a6728
VeDev12512 <ve.cuevasr12512@gmail.com>
2025-07-21 07:55:31 -0400

    Estiliza Create Libros

--- resources/js/Pages/Books/Create.vue
+++ resources/js/Pages/Books/Create.vue
@@ -1,39 +1,60 @@
 <template>
     <title>Create</title>
 
-    <div>
-        <h1>Crear Libros</h1>
-        <form @submit.prevent="submit">
-            <input 
-                v-model="name" type="text" 
-                placeholder="Nombre" name="Nombre" id="Nombre"
-            >
+    <Lauyout>
+        <div>
+            <h1 text-center mb-4 text-white>Crear Libros</h1>
 
-            <input 
-                v-model="publication_date" type="date" 
-                placeholder="Fecha de publicación" name="Fecha de publicación" id="Fecha de publicación"
-            >
+            <form @submit.prevent="submit" class="d-grid gap-3">
+                <input 
+                    v-model="name" 
+                    type="text" 
+                    placeholder="Nombre" name="Nombre" id="Nombre"
+                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                >
 
-            <input 
-                v-model="edition" type="text" 
-                placeholder="Edición" name="Edición" id="Edición"
-            >
+                <input 
+                    v-model="publication_date" 
+                    type="date" 
+                    placeholder="Fecha de publicación" name="Fecha de publicación" id="Fecha de publicación"
+                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                >
 
-            <select v-model="bookAuthors" multiple name="Autor/es" id="Autor/es">
-                <option v-for="author in props.authors" :key="author.id" :value="author.id">
-                    {{ author.first_name }} {{ author.last_name }}
-                </option>
-            </select>
-            <button type="submit">Guardar</button>
+                <input 
+                    v-model="edition" 
+                    type="text" 
+                    placeholder="Edición" name="Edición" id="Edición"
+                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                >
 
-        </form>
-    </div>
+                <select 
+                    v-model="bookAuthors" 
+                    multiple 
+                    name="Autor/es" 
+                    id="Autor/es"
+                    class="form-select bg-dark text-light rounded-3 shadow-sm"
+                >
+                    <option 
+                        v-for="author in props.authors" 
+                        :key="author.id" 
+                        :value="author.id"
+                    >
+                        {{ author.first_name }} {{ author.last_name }}
+                    </option>
+                </select>
+                <button type="submit" class="btn btn-success rounded-pill py-2 px-4">Guardar</button>
+
+            </form>
+        </div>
+    </Lauyout>
 </template>
 
 <script setup>
     //importaciones
     import {ref} from 'vue';
     import {router} from '@inertiajs/vue3';
+    import Lauyout from '../../Layout/Lauyout.vue';
+
 
     //Datos
     const name = ref('');
