a25f9c64e56b6ef0c29cef83ab96409bfe4cd281
VeDev12512 <ve.cuevasr12512@gmail.com>
2025-07-21 22:23:30 -0400

    Agrega mensajes de error a editar autor + crear libro

--- resources/js/Pages/Authors/Edit.vue
+++ resources/js/Pages/Authors/Edit.vue
@@ -6,29 +6,42 @@
             <h1 class="text-center mb-4 text-white">Editar Autor</h1>
 
             <form @submit.prevent="submit" class="d-grid gap-3">
-                <label for="Nombre" class="ml-3 fw-bold fs-5">Nombre</label>
-                <input 
-                    v-model="form.first_name" 
-                    type="text" 
-                    name="Nombre" id="Nombre"
-                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
-                >
-
-                <label for="Apellido" class="ml-3 fw-bold fs-5">Apellido</label>
-                <input 
-                    v-model="form.last_name" 
-                    type="text" 
-                    name="Apellido" id="Apellido"
-                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
-                >
-
-                <label for="País" class="ml-3 fw-bold fs-5">País</label>
-                <input 
-                    v-model="form.country" 
-                    type="text" 
-                    name="País" id="País"
-                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"    
-                >
+                <div>
+                    <label for="Nombre" class="ml-3 fw-bold fs-5">Nombre</label>
+                    <input 
+                        v-model="form.first_name" 
+                        type="text" 
+                        name="Nombre" id="Nombre"
+                        class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                        :class="{'is-invalid': errors.first_name}"
+                    >
+                   <span v-if="errors.first_name" class="invalid-feedback d-block">{{ errors.first_name }}</span>
+                </div>
+                
+                <div>
+                    <label for="Apellido" class="ml-3 fw-bold fs-5">Apellido</label>
+                    <input 
+                        v-model="form.last_name" 
+                        type="text" 
+                        name="Apellido" id="Apellido"
+                        class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                        :class="{'is-invalid': errors.last_name}"
+                    >
+                    <span v-if="errors.last_name" class="invalid-feedback d-block">{{ errors.last_name }}</span> 
+                </div>
+                
+                <div>
+                    <label for="País" class="ml-3 fw-bold fs-5">País</label>
+                    <input 
+                        v-model="form.country" 
+                        type="text" 
+                        name="País" id="País"
+                        class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"    
+                        :class="{'is-invalid': errors.country}"
+                    >     
+                    <span v-if="errors.country" class="invalid-feedback d-block">{{ errors.country }}</span>
+                </div>
+                
 
                 <button type="submit" class="btn btn-success rounded-pill py-2 px-4 ml-3 fw-bold fs-5">Actualizar</button>
 
@@ -41,11 +54,15 @@
 <script setup>
     //importaciones
     import {useForm} from '@inertiajs/vue3';
-    import { Head, Link, router } from '@inertiajs/vue3';
-    import { defineProps } from 'vue';
+    import { Head, Link, router, usePage } from '@inertiajs/vue3';
+    import { defineProps, computed } from 'vue';
     import Lauyout from '../../Layout/Lauyout.vue';
 
     //Datos
+    const errors = computed(
+        () => usePage().props.errors
+    );
+     
     const props = defineProps({
         author: Object
     });
--- resources/js/Pages/Books/Create.vue
+++ resources/js/Pages/Books/Create.vue
@@ -6,45 +6,61 @@
             <h1 class="text-center mb-4 text-white">Crear Libros</h1>
 
             <form @submit.prevent="submit" class="d-grid gap-3">
-                <label for="Nombre" class="ml-3 fw-bold fs-5">Nombre</label>
-                <input 
-                    v-model="name" 
-                    type="text" 
-                    name="Nombre" id="Nombre"
-                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
-                >
-
-                <label for="Fecha de publicación" class="ml-3 fw-bold fs-5">Fecha de publicación</label>
-                <input 
-                    v-model="publication_date" 
-                    type="date" 
-                    name="Fecha de publicación" id="Fecha de publicación"
-                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
-                >
-
-                <label for="Edición" class="ml-3 fw-bold fs-5">Edición</label>
-                <input 
-                    v-model="edition" 
-                    type="text" 
-                    name="Edición" id="Edición"
-                    class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
-                >
-
-                <label for="Autor/es" class="ml-3 fw-bold fs-5">Autor/es</label>
-                <select 
-                    v-model="bookAuthors" 
-                    multiple 
-                    name="Autor/es" id="Autor/es"
-                    class="form-select bg-dark text-light rounded-3 shadow-sm"
-                >
-                    <option 
-                        v-for="author in props.authors" 
-                        :key="author.id" 
-                        :value="author.id"
+                <div>
+                    <label for="Nombre" class="ml-3 fw-bold fs-5">Nombre</label>
+                    <input 
+                        v-model="name" 
+                        type="text" 
+                        name="Nombre" id="Nombre"
+                        class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                        :class="{'is-invalid': errors.name}"
                     >
-                        {{ author.first_name }} {{ author.last_name }}
-                    </option>
-                </select>
+                    <span v-if="errors.name" class="invalid-feedback d-block">{{ errors.name }}</span>
+                </div>
+                
+                <div>
+                    <label for="Fecha de publicación" class="ml-3 fw-bold fs-5">Fecha de publicación</label>
+                    <input 
+                        v-model="publication_date" 
+                        type="date" 
+                        name="Fecha de publicación" id="Fecha de publicación"
+                        class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                        :class="{'is-invalid': errors.publication_date}"
+                    >
+                    <span v-if="errors.publication_date" class="invalid-feedback d-block">{{ errors.publication_date }}</span>
+                </div>
+
+                <div>
+                    <label for="Edición" class="ml-3 fw-bold fs-5">Edición</label>
+                    <input 
+                        v-model="edition" 
+                        type="text" 
+                        name="Edición" id="Edición"
+                        class="form-control rounded-pill bg-dark text-light border-0 shadow-sm"
+                        :class="{'is-invalid': errors.edition}"
+                    >
+                    <span v-if="errors.edition" class="invalid-feedback d-block">{{ errors.edition }}</span>
+                </div>
+                
+                <div>
+                    <label for="Autor/es" class="ml-3 fw-bold fs-5">Autor/es</label>
+                    <select 
+                        v-model="bookAuthors" 
+                        multiple 
+                        name="Autor/es" id="Autor/es"
+                        class="form-select bg-dark text-light rounded-3 shadow-sm"
+                        :class="{'is-invalid': errors.authors}"
+                        >
+                        <option 
+                            v-for="author in props.authors" 
+                            :key="author.id" 
+                            :value="author.id"
+                            >
+                            {{ author.first_name }} {{ author.last_name }}
+                        </option>
+                    </select>
+                    <span v-if="errors.authors" class="invalid-feedback d-block">{{ errors.authors }}</span>
+                </div>
                 <button type="submit" class="btn btn-success rounded-pill py-2 px-4 ml-3 fw-bold fs-5">Guardar</button>
 
             </form>
@@ -54,8 +70,8 @@
 
 <script setup>
     //importaciones
-    import {ref} from 'vue';
-    import { Head, Link, router } from '@inertiajs/vue3';
+    import {ref, computed} from 'vue';
+    import { Head, Link, router, usePage } from '@inertiajs/vue3';
     import Lauyout from '../../Layout/Lauyout.vue';
 
 
@@ -65,22 +81,39 @@
     const edition = ref('');
     const bookAuthors = ref([]);
 
+    const errors = computed(
+        () => usePage().props.errors
+    );
+
     const props = defineProps({
         authors: Array
     });
     
     //funciones
     function submit(){
-        router.post('/books', {
-            name: name.value,
-            publication_date: publication_date.value,
-            edition: edition.value,
-            authors: bookAuthors.value,
-        })
-        console.log( name.value,
-             publication_date.value,
-         edition.value,
-           bookAuthors.value,)
+        router.post('/books', 
+            {
+                name: name.value,
+                publication_date: publication_date.value,
+                edition: edition.value,
+                authors: bookAuthors.value,
+            }, 
+        
+            {
+                onSuccess: () => {
+                    name.value = '';
+                    publication_date = '';
+                    edition = '';
+                    authors = [];
+                }, 
+
+                onError: (errorObject) => {
+                    console.error('Errores de validación:', errorObject);
+                    console.log('Claves de los errores:', Object.keys(errorObject));
+                }
+            }
+        );
+        
     }
 
 </script>
